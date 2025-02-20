0cfdacce13bd91d47e255380f3fa5eccd5eff955
vidusha <dilshanariyarathna1999@gmail.com>
2025-02-21 04:06:01 +0530

    new changes

--- Controller/folderNameController.js
+++ Controller/folderNameController.js
@@ -23,7 +23,7 @@ exports.dirveFile = async(req, res) => {
     try{
         const email = req.params.email
         const {code,message, files} = await folderNameService.dirveFile(email);
-        res.status(200).json({code:code,message:message, files});
+        res.status(200).json({code:code,message:message, files:files});
     }catch(err){
         res.status(500).json ({status:500, message:err.message})
     } 
@@ -33,7 +33,7 @@ exports.folderContent = async(req, res) => {
     try{
         const folderId = req.params.email
         const {code,message, files} = await folderNameService.folderContent(folderId);
-        res.status(200).json({code:code,message:message, files});
+        res.status(200).json({code:code,message:files, files});
     }catch(err){
         res.status(500).json ({status:500, message:err.message})
     } 
