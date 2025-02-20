53f78a2d1492941763a6110fa1c62c8f716cb946
vidusha <dilshanariyarathna1999@gmail.com>
2025-02-20 12:01:58 +0530

    remove admin check

--- Routers/studentRoutes.js
+++ Routers/studentRoutes.js
@@ -9,12 +9,12 @@ router.post('/student/register',studentController.studentRegister);
 router.post('/student/login',studentController.studentLogin);
 router.put('/student/forgotpassword/:email',studentController.studentForgotPassword);
 router.post('/student/checkotp',studentController.studentCheckOtp);
-router.post('/student/give-additional-access', adminCheck, authenticateToken,studentController.giveAditionalFolderAccess);
+router.post('/student/give-additional-access', authenticateToken,studentController.giveAditionalFolderAccess);
 router.put('/student/update/:id',authenticateToken,studentController.studentUpdate);
-router.put('/student/state/update/:id',adminCheck, authenticateToken,studentController.changeStudentState);
+router.put('/student/state/update/:id',authenticateToken,studentController.changeStudentState);
 router.put('/student/updatepassword/:id',authenticateToken,studentController.studentUpdatePassword);
 router.get('/student/getbyid/:id',authenticateToken,studentController.getByStudentId);
-router.get('/student/getallstudents', adminCheck, authenticateToken,studentController.getAllStudent);
+router.get('/student/getallstudents', authenticateToken,studentController.getAllStudent);
 router.delete('/student/deletestudent/:id',authenticateToken,studentController.deleteStudent);
 
 module.exports = router;
