2469a8b467d9de28cf8e7e6caaf5a1736f31f48a
vidusha dilshan <86625992+pgvda@users.noreply.github.com>
2025-08-06 15:53:03 +0530

    Update folderNameRoutes.js

--- Routers/folderNameRoutes.js
+++ Routers/folderNameRoutes.js
@@ -8,8 +8,8 @@ const router = express.Router();
 router.post('/create/folder', authenticateToken,folderNameController.createFolderInDrive);
 router.get('/folder-names',folderNameController.getAllFolderName);
 router.get('/diveFiles/:email',folderNameController.dirveFile);
+router.get('/diveRootFiles/:email',folderNameController.getDirveRootFolders);
 router.get('/folderContent/:id',folderNameController.folderContent);
-router.get('/delete/tokenfile',folderNameController.deleteTokenFile);
 
 
 module.exports = router;
